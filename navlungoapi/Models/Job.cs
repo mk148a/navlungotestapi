1df22861702c31e295ba73633c1652a12512d895
MURAT KANDİL <Mk148a@hotmail.com>
2019-01-11 00:53:49 +0300

    Final

--- navlungoapi/Models/Job.cs
+++ navlungoapi/Models/Job.cs
@@ -1,9 +1,12 @@
-﻿using Newtonsoft.Json;
+﻿using NCrontab;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace navlungoapi.Models
@@ -20,6 +23,7 @@ public class Job
         public string mailadress { get; set; }
         public string schedule { get; set; }
         public string crondesc { get; set; }
+        public string status { get; set; }
         public async Task<string> getcrondesc(string schedule)
         {
             //https://cronexpressiondescriptor.azurewebsites.net/api/descriptor/?expression=0+0+5+*+*&locale=tr-tr
@@ -27,12 +31,50 @@ public class Job
             string result = "";
             using (var client = new WebClient())
             {
-                char[] cronjobparts = schedule.ToCharArray();
+                char[] cronjobparts = schedule.Replace(" ","").ToCharArray();
                 result = await client.DownloadStringTaskAsync(new Uri("https://cronexpressiondescriptor.azurewebsites.net/api/descriptor/?expression=" + cronjobparts[0] + "+" + cronjobparts[1] + "+" + cronjobparts[2] + "+" + cronjobparts[3] + "+" + cronjobparts[4] + "&locale=tr-tr"));
             }
             return result;
         }
+        public string getstatus()
+        {
+               
+            System.Timers.Timer t = new System.Timers.Timer();      
+            var schedule_ = CrontabSchedule.Parse(schedule);
+            DateTime _nextRun = schedule_.GetNextOccurrence(DateTime.Now);
+            DateTime __nextRun = schedule_.GetNextOccurrence(_nextRun);            
+            TimeSpan span = __nextRun.Subtract(_nextRun);         
+                t.Interval = span.TotalMilliseconds;
+                t.Elapsed += T_Elapsed;
+                t.Start();           
+             
+            return status;
+        }
 
+        private void T_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString());
+                string subject = shipper + " test mail job";
+                new SmtpClient
+                {
+                    Host = "mail.secretrithm.com",
+                    Port = 26,
+                    EnableSsl = false,
+                    Timeout = 10000,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("orders@secretrithm.com", "12345")
+                }.Send(new MailMessage { From = new MailAddress("orders@secretrithm.com", "SecretRithm"), To = { mailadress }, Subject = subject, Body = "test message", IsBodyHtml = true, BodyEncoding = Encoding.UTF8 });
+                status = "success";
+            }
+            catch(Exception ee)
+            {
+                System.Diagnostics.Debug.WriteLine(ee.Message);
+               status = "error";
+            }
+        }
 
         public Job(string shipper, string destinationCountry, string mailadress, string schedule)
         {
@@ -40,7 +82,7 @@ public class Job
             this.destinationCountry = destinationCountry;
             this.mailadress = mailadress;
             this.schedule = schedule;
-
+            this.status = getstatus();
             this.crondesc = JsonConvert.DeserializeObject<CronDescApiHelper>(getcrondesc(schedule).Result).description;
         }
     }
